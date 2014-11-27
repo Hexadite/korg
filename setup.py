93e863267b15fbb1509617177b436054ac47daac
Max Braitmaiere <max@hexadite.com>
2014-11-27 11:17:11 +0200

    named version to avoid collision with pypi

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     long_description=open('README.md').read(),
     url='https://github.com/aogaeru/korg',
     name='korg',
-    version='0.0.6',
+    version='0.0.6-hexadite',
     packages=['korg'],
     data_files=[('patterns', find_pattern_files()), 'README.md'],
     install_requires=['regex >= 2013-06-05'],
